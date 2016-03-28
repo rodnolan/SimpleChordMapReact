7adac53977cc3af59d175881b4caf12033933ee7
Rod Nolan <rodnolan@gmail.com>
2016-03-28 08:58:46 -0400

    added the chordmap component
    using smart-css to apply styles.... although I don't know how smart this
    is

---
+++ src/components/chordmap/ChordMap.js
@@ -0,0 +1,171 @@
+"use strict";
+var SmartCSS = require('smart-css');
+var css = new SmartCSS();
+var React = require('react');
+
+var ChordMap = React.createClass({
+
+	render: function() {
+		var keysWrapper = {
+			float: 'left',
+			textAlign: 'center',
+			padding: '20px'
+		};
+
+		var button = {
+			fontSize: '18pt',
+			marginTop: '5px',
+			marginBottom: '5px',
+			paddingRight: '10px',
+			paddingLeft: '10px',
+			fontWeight: 'bold',
+			color: 'navy'
+		};
+
+		var mapWrapper = {
+			position: 'relative',
+			float: 'left',
+			backgroundImage: 'url("./images/SimpleChordMapBackground.png")',
+			height: 668,
+			width: 281
+		};
+
+		css.setClass('.chordLetter', { 
+			position: 'absolute', 
+			fontWeight: 'bold', 
+			fontFamily: 'monospace'
+		}, 
+		{className: 'chordLetter'});
+
+		css.setClass('.rootChord', {
+			color: 'DarkBlue', 
+			fontSize: '124px'
+		}, {className: 'rootChord'});
+		
+		css.setClass('.majorChord', {
+			color: 'blue',
+			fontSize: '72px'
+		}, {className: 'majorChord'});
+		
+		css.setClass('.minorChord', {
+			color: 'brown',
+			fontSize: '48px'
+		}, {className: 'minorChord'});
+
+		css.setClass('.col1', { left: '50px' }, {className: 'col1'});
+		css.setClass('.col2', { left: '175px' }, {className: 'col2'});
+
+		css.setClass('.row1', { top: '30px' }, {className: 'row1'});
+		css.setClass('.row2', { top: '90px' }, {className: 'row2'});
+		css.setClass('.row3', { top: '165px' }, {className: 'row3'});
+		css.setClass('.row4', { top: '225px' }, {className: 'row4'});
+		css.setClass('.row5', { top: '265px' }, {className: 'row5'});
+		css.setClass('.row6', { top: '350px' }, {className: 'row6'});
+		css.setClass('.row7', { top: '400px' }, {className: 'row7'});
+		css.setClass('.row8', { top: '500px' }, {className: 'row8'});
+		
+		SmartCSS.injectStyles();
+
+		return (
+			<div>
+				<div style={keysWrapper}>
+					<button style={button}>C</button><br />
+					<button style={button}>C#</button>
+					<button style={button}>Db</button><br />
+					<button style={button}>D</button><br />
+					<button style={button}>D#</button>
+					<button style={button}>Eb</button><br />
+					<button style={button}>E</button><br />
+					<button style={button}>F</button><br />
+					<button style={button}>F#</button>
+					<button style={button}>Gb</button><br />
+					<button style={button}>G</button><br />
+					<button style={button}>G#</button>
+					<button style={button}>Ab</button><br />
+					<button style={button}>A</button><br />
+					<button style={button}>A#</button>
+					<button style={button}>Bb</button><br />
+					<button style={button}>B</button><br />
+				</div>
+				
+				<div style={mapWrapper}>
+					<div id="chord_two_1"
+						className={css.getClasses({
+							chordLetter: true,
+							col1: true,
+							row1: true,
+							minorChord: true
+						})}>
+					2m
+					</div>
+					<div id="chord_five_1"
+						className={css.getClasses({
+							chordLetter: true,
+							col2: true,
+							row2: true,
+							majorChord: true
+						})}>
+					5
+					</div>
+					<div id="chord_three"
+						className={css.getClasses({
+							chordLetter: true,
+							col1: true,
+							row3: true,
+							minorChord: true
+						})}>
+					3m
+					</div>
+					<div id="chord_six"
+						className={css.getClasses({
+							chordLetter: true,
+							col2: true,
+							row4: true,
+							minorChord: true
+						})}>
+					6m
+					</div>
+					<div id="chord_four"
+						className={css.getClasses({
+							chordLetter: true,
+							col1: true,
+							row5: true,
+							majorChord: true
+						})}>
+					4
+					</div>
+					<div id="chord_two_2"
+						className={css.getClasses({
+							chordLetter: true,
+							col2: true,
+							row6: true,
+							minorChord: true
+						})}>
+					2m
+					</div>
+					<div id="chord_five_2"
+						className={css.getClasses({
+							chordLetter: true,
+							col1: true,
+							row7: true,
+							majorChord: true
+						})}>
+					5
+					</div>
+					
+					<div id="chord_root"
+						className={css.getClasses({
+							chordLetter: true, 
+							col1: true, 
+							row8: true,
+							rootChord: true
+						})}>
+					1
+					</div>
+				</div>
+			</div>
+		);
+	}
+});
+
+module.exports = ChordMap;
--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,15 +1,18 @@
 "use strict";
 
 var React = require('react');
-
+var ChordMap = require('./chordmap/chordMap');
 var HomePage = React.createClass({
 
 	render: function() {
 		return (
 			// JSX can't use class="" because that's a reserved word... the compiled html will be class="" though
-			<div className="jumbotron"> 
-				<h1>Simple Chord Map</h1>
-				<p>visualize chord progressions that work</p>
+			<div>
+				<div className="jumbotron"> 
+					<h2>Simple Chord Map</h2>
+					<p>visualize chord progressions that work</p>
+				</div>
+				<ChordMap selectedKey="C" />
 			</div>
 		);
 	}
