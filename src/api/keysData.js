dbb9c0d1864bc3bb650356591d4e2dce21948a19
Rod Nolan <rodnolan@gmail.com>
2016-03-28 15:01:50 -0400

    mock api setup
    added lodash for use in the mock api and added some hard coded data to
    represent the keys

---
+++ src/api/keysData.js
@@ -0,0 +1,53 @@
+module.exports = {
+	chromaticScale: 
+	[
+		{
+			id: '0', 
+			names: ['C']
+		},	
+		{
+			id: '1', 
+			names: ['C#', 'Db']
+		},	
+		{
+			id: '2', 
+			names: ['D']
+		},	
+		{
+			id: '3', 
+			names: ['D#', 'Eb']
+		},	
+		{
+			id: '4', 
+			names: ['E']
+		},	
+		{
+			id: '5', 
+			names: ['F']
+		},	
+		{
+			id: '6', 
+			names: ['F#', 'Gb']
+		},	
+		{
+			id: '7', 
+			names: ['G']
+		},	
+		{
+			id: '8', 
+			names: ['G#', 'Ab']
+		},	
+		{
+			id: '9', 
+			names: ['A']
+		},	
+		{
+			id: '10', 
+			names: ['A#', 'Bb']
+		},	
+		{
+			id: '11', 
+			names: ['B']
+		}
+	]
+};
